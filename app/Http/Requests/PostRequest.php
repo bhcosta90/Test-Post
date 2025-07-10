b0de0e39d34349fb0df9ed36c042676cd8a60ca8
Bruno Costa <bhcosta90@gmail.com>
2025-07-10 09:08:32 -0300

    Add status validation to PostRequest using PostStatusEnum

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -4,7 +4,9 @@ declare(strict_types = 1);
 
 namespace App\Http\Requests;
 
+use App\Models\Enum\PostStatusEnum;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 final class PostRequest extends FormRequest
 {
@@ -13,6 +15,7 @@ final class PostRequest extends FormRequest
         return [
             'user_id' => ['required', 'exists:users,id'],
             'title'   => ['required'],
+            'status'  => ['required', Rule::enum(PostStatusEnum::class)],
         ];
     }
 
