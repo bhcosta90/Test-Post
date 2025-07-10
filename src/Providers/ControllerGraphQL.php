5729b85bfc79be233c2e8ed0971f57de6ee9be00
Bruno Costa <bhcosta90@gmail.com>
2025-07-10 08:16:37 -0300

    Add GraphQL controller and configuration file

---
+++ src/Providers/ControllerGraphQL.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace QuantumTecnology\ControllerQraphQLExtension\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ControllerGraphQL extends ServiceProvider
+{
+    public function register(): void
+    {
+
+    }
+
+    public function boot(): void {}
+}
