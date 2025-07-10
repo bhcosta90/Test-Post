bbfeafba1d0223848a893859795c2f169246d05f
Bruno Costa <bhcosta90@gmail.com>
2025-07-10 15:06:35 -0300

    Add Telescope integration and update configuration files

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,8 +1,7 @@
 <?php
 
-declare(strict_types = 1);
-
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     QuantumTecnology\ControllerQraphQLExtension\Providers\ControllerGraphQL::class,
 ];
