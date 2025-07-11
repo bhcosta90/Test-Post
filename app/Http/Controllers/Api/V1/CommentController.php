c1d2f010b9faca91b39fb510049a53316b1413d3
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-07-11 12:49:40 -0300

    Refactor migration and controller files to enforce strict types and improve code clarity

--- app/Http/Controllers/Api/V1/CommentController.php
+++ app/Http/Controllers/Api/V1/CommentController.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
 use App\Models\Comment;
 use Illuminate\Database\Eloquent\Model;
 use QuantumTecnology\ControllerQraphQLExtension\Traits\AsApiController;
