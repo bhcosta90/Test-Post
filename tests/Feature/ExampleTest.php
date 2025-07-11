11ef0b23694a2e9fac5908624330ccba0d1ddeb0
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-07-11 08:10:20 -0300

    Refactor test setup and configuration for improved database handling in tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,5 @@ declare(strict_types = 1);
 
 test('the application returns a successful response', function (): void {
     $response = $this->get('/');
-
     $response->assertStatus(200);
 });
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,5 +8,16 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $dbPath = base_path(env('DB_DATABASE', 'testing'));
+
+        if (!file_exists($dbPath)) {
+            touch($dbPath);
+        }
+
+        $this->artisan('migrate');
+    }
 }
