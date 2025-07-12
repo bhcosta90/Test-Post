2dbd4632159b3c06738cd179e89bd6bac99b0206
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-07-12 08:59:37 -0300

    Update provider reference for ControllerBasicsExtension

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,5 +5,5 @@ declare(strict_types = 1);
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
-    QuantumTecnology\ControllerBasicsExtension\Providers\ControllerGraphQL::class,
+    QuantumTecnology\ControllerBasicsExtension\Providers\ControllerBasicsExtensionProvider::class,
 ];
